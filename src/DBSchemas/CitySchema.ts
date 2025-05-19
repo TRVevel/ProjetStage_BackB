feb85d158c6ceb261cd0e50165d16fbf76206387
TRVevel <piscionequentin@gmail.com>
2025-05-19 14:44:41 +0200

    Db Ok

--- src/DBSchemas/CitySchema.ts
+++ src/DBSchemas/CitySchema.ts
@@ -14,9 +14,10 @@ const CitySchema: Schema = new Schema({
     ville_departement: { type: String, required: true },
     ville_nom: { type: String, required: true },
     ville_nom_simple: { type: String, required: true },
-    publishedYear: { type: Number, required: true },
+    ville_nom_reel: { type: String, required: true },
     ville_code_postal: { type: String, required: true},
+    
 });
-
+CitySchema.index({ ville_nom_simple: 1 });
 // Exporter le modèle
 export default mongoose.model<ICity>('City', CitySchema);
